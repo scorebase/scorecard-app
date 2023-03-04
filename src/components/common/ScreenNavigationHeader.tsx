8e6f71a084c6f387bad0ef4ac759113eb8f71a47
jjuggy <amidavid01@gmail.com>
2023-03-05 00:00:23 +0100

    deleted unneeded package and removed the icon from the screenheadernavigation

--- src/components/common/ScreenNavigationHeader.tsx
+++ src/components/common/ScreenNavigationHeader.tsx
@@ -1,29 +1,22 @@
 import { Pressable, StyleSheet, Text, View } from "react-native";
 import React from "react";
 import { useNavigation } from "@react-navigation/native";
-import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
-import { CaretLeft, Info } from "phosphor-react";
 import { Ionicons } from "@expo/vector-icons";
 
 const ScreenNavigationHeader = ({ backTo, middleComponent, moveTo }) => {
   const navigation = useNavigation();
   return (
     <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
-      <Pressable onPress={() => navigation.navigate(backTo as never)}>
-        <Ionicons name="ios-arrow-back-outline" size={24} color="black" />
-      </Pressable>
-      <View>
-        <Text style={{ fontWeight: "600", fontSize: 17 }}>
-          {middleComponent}
-        </Text>
+      <View style={{ width: '57%', flexDirection: 'row', justifyContent: "space-between", }}>
+        <Pressable onPress={() => navigation.navigate(backTo as never)}>
+          <Ionicons name="ios-arrow-back-outline" size={24} color="black" />
+        </Pressable>
+        <View>
+          <Text style={{ fontWeight: "600", fontSize: 17 }}>
+            {middleComponent}
+          </Text>
+        </View>
       </View>
-      <Pressable onPress={() => navigation.navigate(moveTo as never)}>
-        <Ionicons
-          name="md-information-circle-outline"
-          size={24}
-          color="black"
-        />
-      </Pressable>
     </View>
   );
 };
