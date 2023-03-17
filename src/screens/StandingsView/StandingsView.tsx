460ce6091332f44f6f9ca32619af7b35253f874c
jjuggy <amidavid01@gmail.com>
2023-03-17 19:18:21 +0100

    added the network error UI to the standings screen

--- src/screens/StandingsView/StandingsView.tsx
+++ src/screens/StandingsView/StandingsView.tsx
@@ -8,14 +8,47 @@ export interface IMembers {
   StandingId: number, TeamId: number, draws: number, goals_conceded: number, goals_scored: number, losses: number, points: number, wins: number
 }
 const StandingsView = () => {
-  const [standingsResponse, setStadingsResponse, isLoading, refetch] = useAxios('https://scorecard-be.herokuapp.com/standing')
+  const [standingsResponse, isNetworkError, isLoading, refetch] = useAxios('https://scorecard-be.herokuapp.com/standing')
 
   useFocusEffect(
     React.useCallback(() => {
       refetch();
     }, [])
   );
+  if (isNetworkError) {
+    return (
+      <View style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }
+      }>
+        <View style={{
+          backgroundColor: '#F7CAC9',
+          padding: 10,
+          borderRadius: 5,
+          alignSelf: 'stretch',
+          marginHorizontal: 20,
+          shadowColor: '#000',
+          shadowOffset: {
+            width: 0,
+            height: 2,
+          }, shadowOpacity: 0.25,
+          shadowRadius: 3.84,
+          elevation: 5,
+        }}>
 
+          <Text style={{
+            color: '#6D4C41',
+            fontWeight: 'bold',
+            textAlign: 'center',
+          }}>
+            Network error: please check your internet connection and try again.
+          </Text>
+        </View>
+      </View >
+    )
+  }
   // console.log('stndng res', standingsResponse)
   return (
     <View style={{ marginTop: 20, padding: 28 }}>
