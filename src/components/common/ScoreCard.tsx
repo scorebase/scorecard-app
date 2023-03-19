bd952f65d0029ba08f7f5d32c18e5cd025a00cb4
jjuggy <amidavid01@gmail.com>
2023-03-19 13:44:55 +0100

    edited the score card to show the balls for the away team to and also show the team name playing o ntheir logos

--- src/components/common/ScoreCard.tsx
+++ src/components/common/ScoreCard.tsx
@@ -5,10 +5,16 @@ import { FontAwesome } from '@expo/vector-icons';
 export const ScoreCard = ({ homeTeam, awayTeam, homeScore, awayScore, header, homeTeamScorers, awayTeamScorers }) => {
 
     const homeTeamScorerMap = {}
+    const awayTeamScorermap = {}
     homeTeamScorers.forEach((scorer) => {
         if (homeTeamScorerMap[scorer.player.name]) {
             homeTeamScorerMap[scorer.player.name]++
         } else homeTeamScorerMap[scorer.player.name] = 1
+    })
+    awayTeamScorers.forEach((scorer) => {
+        if (awayTeamScorermap[scorer.player.name]) {
+            awayTeamScorermap[scorer.player.name]++
+        } else awayTeamScorermap[scorer.player.name] = 1
     })
     const FootballIcons = (numberOfGoals: number) => {
         const ballsIcons = []
@@ -25,16 +31,22 @@ export const ScoreCard = ({ homeTeam, awayTeam, homeScore, awayScore, header, ho
         <View style={{ height: 200, width: '100%', marginTop: 20, padding: 12 }}>
             <Text style={{ textAlign: 'center', marginVertical: 15, fontWeight: '500' }}>{header}</Text>
             <View style={{ flexDirection: "row", justifyContent: 'space-between', width: '90%', alignSelf: "center", alignItems: 'center' }}>
-                <View><Image source={{ uri: homeTeam.logo }} style={{ width: 55, height: 55 }} /></View>
+                <View style={{ alignItems: 'center',}}>
+                    <Text style ={{fontWeight:'700',fontSize:20}}>{homeTeam.short_name}</Text>
+                    <Image source={{ uri: homeTeam.logo }} style={{ width: 55, height: 55 }} />
+                </View>
                 <Text style={{ fontSize: 25, fontWeight: '700' }}>{homeScore}{' '} - {' '}{awayScore}</Text>
-                <View><Image source={{ uri: awayTeam.logo }} style={{ width: 55, height: 55 }} /></View>
+                <View style={{ justifyContent: "center", alignItems: "center" }}>
+                    <Text style ={{fontWeight:"700",fontSize:20}}>{awayTeam.short_name}</Text>
+                    <Image source={{ uri: awayTeam.logo }} style={{ width: 55, height: 55 }} />
+                </View>
             </View>
             <View style={{ flexDirection: 'row', justifyContent: "space-between", marginTop: 8, width: "90%", alignSelf: "center" }}>
                 <View style={{ justifyContent: 'center' }}>
                     {Object.entries(homeTeamScorerMap).map((scorer: any) => <Text key={scorer.id} style={styles.scorer}>{scorer[0]} <View>{FootballIcons(scorer[1])}</View> </Text>)}
                 </View>
                 <View>
-                    {awayTeamScorers.map((scorer) => <Text key={scorer.player_id} style={styles.scorer}>{scorer.player.name}</Text>)}
+                    {Object.entries(awayTeamScorermap).map((scorer: any) => <Text key={scorer.id} style={styles.scorer}>{scorer[0]} <View>{FootballIcons(scorer[1])}</View> </Text>)}
                 </View>
             </View>
         </View>
